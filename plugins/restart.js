1e58b8010365a9ded3c9a327b6d13486123dae57
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-23 21:12:34 +0530

    Create restart.js

---
+++ plugins/restart.js
@@ -0,0 +1,27 @@
+const got = require("got");
+const Heroku = require("heroku-client");
+const { command, isPrivate, tiny } = require("../lib/");
+const Config = require("../config");
+const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
+const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
+const simpleGit = require("simple-git");
+const { secondsToDHMS } = require("../lib");
+const git = simpleGit();
+const exec = require("child_process").exec;
+
+
+command(
+  {
+    pattern: "restart",
+    fromMe: true,
+    type: "heroku",
+    desc: "Restart Dyno",
+    type: "heroku",
+  },
+  async (message) => {
+    await message.sendMessage(`_Restarting_`);
+    await heroku.delete(baseURI + "/dynos").catch(async (error) => {
+      await message.sendMessage(`HEROKU : ${error.body.message}`);
+    });
+  }
+);
