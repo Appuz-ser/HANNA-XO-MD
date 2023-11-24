ffe977a1d1d415f015f1ffc9b1a81fbb19d39848
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 05:24:10 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -8,3 +8,24 @@ you may not use this file except in compliance with the License.
 HANNA-XO-MD - Abhiiyh
 */
 
+
+command(
+  {
+    pattern: "gjid",
+    fromMe: true,
+    desc: "gets jid of all group members",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("_This command is for groups_");
+    let { participants } = await client.groupMetadata(message.jid);
+    let participant = participants.map((u) => u.id);
+    let str = "╭──〔 *Group Jids* 〕\n";
+    participant.forEach((result) => {
+      str += `├ *${result}*\n`;
+    });
+    str += `╰──────────────`;
+    message.reply(str);
+  }
+);
