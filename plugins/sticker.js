58462e61742caf01d8dc01987a42f14f49415295
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-24 15:50:31 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,4 +1,4 @@
->const config = require("../config");
+const config = require("../config");
 const { command, isPrivate, getJson, sleep, tiny } = require("../lib/");
 const { Image } = require("node-webpmux");
 
