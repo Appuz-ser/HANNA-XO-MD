e883cd15623de061e81d5a151af918c93f31b0ef
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 05:22:32 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -1,6 +1,9 @@
-
 /* Copyright (C) 2023 Abhiiyh.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 HANNA-XO-MD - Abhiiyh
 */
+
+const config = require("../config");
+const { command, isPrivate } = require("../lib/");
+const { isAdmin, parsedJid, isUrl } = require("../lib");
