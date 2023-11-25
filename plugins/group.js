68f5f828b8e6dad9ab79de80ccee3b89d7f9f54d
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 07:07:34 +0530

    Update group.js

--- plugins/group.js
+++ plugins/group.js
@@ -106,23 +106,3 @@ command(
   }
 );
 
-/* Copyright (C) 2023 Abhiiyh.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-HANNA-XO-MD - Abhiiyh
-*/
-
-
-command(
-  {
-    pattern: "jid",
-    fromMe: true,
-    desc: "Give jid of chat/user",
-    type: "user",
-  },
-  async (message, match) => {
-    return await message.sendMessage(
-      message.mention[0] || message.reply_message.jid || message.jid
-    );
-  }
-);
