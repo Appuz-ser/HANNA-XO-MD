b6a912be9a4328012127157fc71f154f5e3db078
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 13:50:50 +0530

    alive settted 🍉🍉🍉

---
+++ plugins/alive.js
@@ -0,0 +1,13 @@
+const { runtime, isPrivate } = require("../lib");
+
+command(
+	{
+		pattern: "he",
+		desc: "Alive Bot",
+		fromMe: true,
+		type: "user"
+},
+async (conn, match) => {
+const sender = m.pushName
+	await conn.reply(`𝐈’𝐦 𝐀𝐥𝐢𝐯𝐞 𝐁𝐫𝐨 ${sender}`)
+});
