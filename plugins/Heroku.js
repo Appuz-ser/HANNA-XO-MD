8791c7bcd6fb246162eb1690e2231f36bd2759ef
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 07:36:35 +0530

    Update Heroku.js

--- plugins/Heroku.js
+++ plugins/Heroku.js
@@ -1,1 +1,18 @@
+const got = require("got");
+const Heroku = require("heroku-client");
+const { command, isPrivate, tiny } = require("../lib/");
+const Config = require("../config");
+const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
+const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
+const simpleGit = require("simple-git");
+const { secondsToDHMS } = require("../lib");
+const git = simpleGit();
+const exec = require("child_process").exec;
+
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
 
