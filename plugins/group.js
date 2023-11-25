cdbf9f0191ec7407d5ec9dfd3387f5f469912d3e
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 06:48:23 +0530

    Add jid,mute,unmute,tagall

--- plugins/group.js
+++ plugins/group.js
@@ -29,3 +29,80 @@ command(
     message.reply(str);
   }
 );
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
+
+
+command(
+  {
+    pattern: "tagall ?(.*)",
+    fromMe: true,
+    desc: "mention all users in group",
+    type: "group",
+  },
+  async (message, match) => {
+    if (!message.isGroup) return;
+    const { participants } = await message.client.groupMetadata(message.jid);
+    let teks = "";
+    for (let mem of participants) {
+      teks += ` ➛ @${mem.id.split("@")[0]}\n`;
+    }
+    message.sendMessage(teks.trim(), {
+      mentions: participants.map((a) => a.id),
+    });
+  }
+);
+
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
+
+
+command(
+  {
+    pattern: "mute",
+    fromMe: true,
+    desc: "mute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("_This command is for groups_");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("_I'm not admin_");
+    await message.reply("*Muted*");
+    return await client.groupSettingUpdate(message.jid, "announcement");
+  }
+);
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
+
+
+command(
+  {
+    pattern: "unmute",
+    fromMe: true,
+    desc: "unmute group",
+    type: "group",
+  },
+  async (message, match, m, client) => {
+    if (!message.isGroup)
+      return await message.reply("_This command is for groups_");
+    if (!isAdmin(message.jid, message.user, message.client))
+      return await message.reply("_I'm not admin_");
+    await message.reply("*Unmuting*");
+    return await client.groupSettingUpdate(message.jid, "not_announcement");
+  }
+);
+
