17e29ae6b1e51ffa26f15effa3d3660bdc9ecf0b
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 08:06:13 +0530

    Update runtime.js

--- plugins/runtime.js
+++ plugins/runtime.js
@@ -1,4 +1,11 @@
->const { runtime, isPrivate } = require("../lib");
+const { runtime, isPrivate } = require("../lib");
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
+
 
 command(
 	{
