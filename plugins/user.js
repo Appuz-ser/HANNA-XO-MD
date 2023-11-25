be690e4604ca179b2f8fb0444afc4df4d5050b80
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-11-25 08:16:56 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -20,3 +20,10 @@ command(
     );
   }
 );
+
+
+/* Copyright (C) 2023 Abhiiyh.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+HANNA-XO-MD - Abhiiyh
+*/
