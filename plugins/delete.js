4edfc39e939b4b1ca005e083fe7c9ce6c3aab45f
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-12-12 08:11:32 +0530

    Create delete.js

---
+++ plugins/delete.js
@@ -0,0 +1,21 @@
+>const { command } = require("../lib");
+
+/* Copyright (C) 2022 X-Electra.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+X-Asena - X-Electra
+*/
+
+command(
+  {
+    pattern: "dlt",
+    fromMe: true,
+    desc: "deletes a message",
+    type: "user",
+  },
+  async (message, match,m,client) => {
+    if (message.isGroup) {
+      client.sendMessage(message.jid, { delete: message.reply_message.key })
+    }
+  }
+);
