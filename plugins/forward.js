a9ba604730a7d95062f3425ea6fd1a55b2426647
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2023-12-31 07:19:08 +0530

    Create forward.js

---
+++ plugins/forward.js
@@ -0,0 +1,17 @@
+const { command, parsedJid } = require("../lib/");
+
+command(
+  {
+    pattern: "forward",
+    fromMe: true,
+    desc: "Forwards the replied Message",
+    type: "user",
+  },
+  async (message, match, m) => {
+    if(!m.quoted) return message.reply('Reply to something') 
+    let jids = parsedJid(match);
+    for (let i of jids) {
+      await message.forward(i, message.reply_message.message);
+    }   
+  }
+);
