b586252c58500e46b4ccb4460189f5e9528008d0
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2024-01-02 17:34:39 +0530

    Create dyno.js

---
+++ plugins/dyno.js
@@ -0,0 +1,46 @@
+const got = require("got");
+const Heroku = require("heroku-client");
+const { command, isPrivate, tiny } = require("../lib/");
+const Config = require("../config");
+const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
+const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
+const simpleGit = require("simple-git");
+const { secondsToDHMS } = require("../lib");
+const git = simpleGit();
+const exec = require("child_process").exec;
+command(
+  {
+    pattern: "dyno",
+    fromMe: isPrivate,
+    desc: "Show Quota info",
+    type: "heroku",
+  },
+  async (message) => {
+    try {
+      heroku
+        .get("/account")
+        .then(async (account) => {
+          const url = `https://api.heroku.com/accounts/${account.id}/actions/get-quota`;
+          headers = {
+            "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
+            Authorization: "Bearer " + Config.HEROKU_API_KEY,
+            Accept: "application/vnd.heroku+json; version=3.account-quotas",
+          };
+          const res = await got(url, { headers });
+          const resp = JSON.parse(res.body);
+          const total_quota = Math.floor(resp.account_quota);
+          const quota_used = Math.floor(resp.quota_used);
+          const remaining = total_quota - quota_used;
+          const quota = `Total Quota : ${secondsToDHMS(total_quota)}
+Used  Quota : ${secondsToDHMS(quota_used)}
+Remaning    : ${secondsToDHMS(remaining)}`;
+          await message.sendMessage("```" + quota + "```");
+        })
+        .catch(async (error) => {
+          return await message.sendMessage(`HEROKU : ${error.body.message}`);
+        });
+    } catch (error) {
+      await message.sendMessage(error);
+    }
+  }
+);
