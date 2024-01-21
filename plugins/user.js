68a7aafef4bb96bbf69bab897fb1b61594e50c2b
Abhiiiyh <127591148+ABHIIY-BRO@users.noreply.github.com>
2024-01-21 20:53:24 +0530

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -28,27 +28,3 @@ HANNA-XO-MD - Abhiiyh
 */
 
 
-command(
-  {
-    pattern: "block",
-    fromMe: true,
-    desc: "Block a person",
-    type: "user",
-  },
-  async (message, match) => {
-    if (message.isGroup) {
-      let jid = message.mention[0] || message.reply_message.jid;
-      if (!jid) return await message.reply("_Reply to a person or mention_");
-      await message.block(jid);
-      return await message.sendMessageMessage(
-        `_@${jid.split("@")[0]} Blocked_`,
-        {
-          mentions: [jid],
-        }
-      );
-    } else {
-      await message.block(message.jid);
-      return await message.reply("_User blocked_");
-    }
-  }
-);
