ea982fac0f937191edf40164300cb99a6008813c
BaDaN.-.SeR <100457004+Kingbadan321@users.noreply.github.com>
2024-01-24 00:30:14 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -8,7 +8,7 @@ command(
         pattern: "insta",
         fromMe: isPrivate,
         desc: "Instagram Video/Photo Downloader",
-        type: "downloder",
+        type: "downloader",
     },
     async (message, match) => {
     	
